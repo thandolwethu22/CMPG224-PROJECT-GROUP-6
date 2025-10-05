7feecbabba38dcdc7b89a1500220f7b64fed14dd
wantedProgrammer <tanakasedze7@gmail.com>
2025-10-05 19:14:57 +0200

    Creation of maven project with spring dependencies

---
+++ csvts/src/main/java/com/nwu/csvts/CsvtsApplication.java
@@ -0,0 +1,13 @@
+package com.nwu.csvts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvtsApplication.class, args);
+	}
+
+}
---
+++ csvts/src/test/java/com/nwu/csvts/CsvtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwu.csvts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
